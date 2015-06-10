7a23e9eb5de0c68133119c321f11d0b3de899729
HisJuneIsX <nerv_2192@live.com>
2015-06-10 09:23:17 +0800

    XHLH

---
+++ XHLH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XHLH.Startup))]
+namespace XHLH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
